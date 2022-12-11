e3036b38c753faa90a7bf8d887224732b2226bce
Félix Bouquet <fbouquet@student.42.fr>
2022-12-11 02:05:08 -0500

    dismount effetc fades

--- lib/objects/dismount_effect.py
+++ lib/objects/dismount_effect.py
@@ -6,6 +6,7 @@ class   DismountEffect:
     @classmethod
     def build_class(cls, env, dimensions):
         cls.img = pygame.transform.scale(pygame.image.load(env.img_folder + 'objects/' + 'dismount_effect.png'), (dimensions, dimensions))
+        cls.img_fades = pygame.transform.scale(pygame.image.load(env.img_folder + 'objects/' + 'dismount_effect_fades.png'), (dimensions, dimensions))
         return cls
 
     def __init__(self, x, y):
@@ -14,10 +15,13 @@ class   DismountEffect:
         self.time = 0
 
     def display(self, env):
-        env.mod.tools.display(env, self.img, self.x, self.y)
+        if self.time > 9:
+            env.mod.tools.display(env, self.img_fades, self.x, self.y)
+        else:
+            env.mod.tools.display(env, self.img, self.x, self.y)
 
     def update(self):
         self.time += 1
-        if self.time > 9:
+        if self.time > 21:
             self.disappear = True
 
